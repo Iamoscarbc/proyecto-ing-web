f44dc1c87ece329262ea593bf3d0b83032a61d69
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-05-14 16:51:00 -0500

    gitignore

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/iamoscarbc/proyecto-ing-web.git master:gh-pages
 
 cd -
